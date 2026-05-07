36366d6975da4a05c001de1e00e36528952cd380
Tilman Holube <tilman@holube.de>
2026-05-07 14:09:43 +0200

    Trying to install Prusti and Aeneas

--- .dev/verify-all.sh
+++ .dev/verify-all.sh
@@ -4,41 +4,60 @@ set -e
 
 #echo "Verifying Aeneas Examples"
 #cd Aeneas
+#cargo --version
+#charon --version
+#aeneas --version
 #charon cargo --preset=aeneas --dest-file=aeneas_test.llbc
 #aeneas -backend lean aeneas_test.llbc -dest proofs -subdir /AeneasTest/Code -split-files -namespace AeneasTest
-#cd proofs && lake build
+#cd proofs
+#lake --version
+#lake build
 #cd ../..
-#echo "Successfully verified Aeneas Examples"
+#echo "Successfully verified Aeneas Examples\n\n"
 
 echo "Verifying Creusot Examples"
 cd Creusot
+cargo --version
 cargo creusot init
 cargo creusot prove
 cd ..
-echo "Successfully verified Creusot Examples"
+echo "Successfully verified Creusot Examples\n\n"
 
 echo "Verifying Flux Examples"
 cd Flux
+cargo --version
+cargo flux --version
 cargo flux
 cd ..
-echo "Successfully verified Flux Examples"
+echo "Successfully verified Flux Examples\n\n"
 
 echo "Verifying Kani Examples"
 cd Kani
+cargo --version
+cargo kani --version
 cargo kani
 cd ..
-echo "Successfully verified Kani Examples"
+echo "Successfully verified Kani Examples\n\n"
+
+echo "Verifying Prusti Examples"
+cd Prusti
+cargo --version
+cargo prusti
+cd ..
+echo "Successfully verified Prusti Examples\n\n"
 
 echo "Verifying VeriFast Examples"
 cd VeriFast
+cargo --version
 cargo verifast
 cd ..
-echo "Successfully verified VeriFast Examples"
+echo "Successfully verified VeriFast Examples\n\n"
 
 echo "Verifying Verus Examples"
 cd Verus
+cargo --version
 cargo verus verify
 cd ..
-echo "Successfully verified Verus Examples"
+echo "Successfully verified Verus Examples\n\n"
 
 echo "Successfully verified all Examples"
