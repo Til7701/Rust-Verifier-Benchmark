ea041a6bff77575989ce1cb422a05a49de9ae5d4
Tilman Holube <tilman@holube.de>
2026-05-09 22:36:43 +0200

    Aeneas somewhat working

--- .dev/verify-all.sh
+++ .dev/verify-all.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-#echo "Verifying Aeneas Examples"
-#cd Aeneas
-#cargo --version
-#charon --version
-#aeneas --version
-#charon cargo --preset=aeneas --dest-file=aeneas_test.llbc
-#aeneas -backend lean aeneas_test.llbc -dest proofs -subdir /AeneasTest/Code -split-files -namespace AeneasTest
-#cd proofs
-#lake --version
-#lake build
-#cd ../..
-#echo "Successfully verified Aeneas Examples\n\n"
+echo "Verifying Aeneas Examples"
+cd Aeneas
+cargo --version
+charon version
+aeneas -version
+charon cargo --preset=aeneas --dest-file=aeneas_test.llbc
+aeneas -backend lean aeneas_test.llbc -dest proofs -subdir /AeneasTest/Code -split-files -namespace AeneasTest
+cd proofs
+lake --version
+lake build
+cd ../..
+echo "Successfully verified Aeneas Examples\n\n"
 
 echo "Verifying Creusot Examples"
 cd Creusot
--- Aeneas/proofs/AeneasTest/Properties/Basic.lean
+++ Aeneas/proofs/AeneasTest/Properties/Basic.lean
@@ -1,18 +1,23 @@
 import Aeneas
 import AeneasTest.Code.Funs
 
+open Aeneas
+open Aeneas.Std
+
 open AeneasTest
 
 #setup_aeneas_simps
 
 @[step]
-theorem add_spec (a b : Std.U32) (h : a.val + b.val < Std.U32.max) :
+theorem add_spec (a b : U32) (h : a.val + b.val < U32.max) :
   add a b ⦃ c => c.val = a.val + b.val ⦄ := by
   unfold add
   step
+  simp_all
 
 @[step]
-theorem octuple_spec (x1 : Std.I8) (h : -16#i8 ≤ x1.val && x1.val < 16#i8) :
+theorem octuple_spec (x1 : I8) (h : -16#i8 ≤ x1.val && x1.val < 16#i8) :
   octuple.octuple x1 ⦃ x8 => x8.val = x1.val * 8#i8 ⦄ := by
-  unfold octuple
-  step
+  unfold octuple.octuple
+  step*
+  simp_all
