0ffc57df1cc561ad52a0924a93a420979d9fec81
Tilman Holube <tilman@holube.de>
2026-06-09 19:28:28 +0200

    Add Flux implementation of ternary_logic

--- Flux/src/ternary_logic.rs
+++ Flux/src/ternary_logic.rs
@@ -1,22 +1,40 @@
 use flux_rs::assert;
 use flux_rs::attrs::*;
-use std::ops::Not;
+use std::ops::{BitAnd, BitOr, Not};
 
-#[refined_by(value: int)]
-#[derive(Copy, Clone, PartialEq, Eq)]
+defs!(
+    fn trit_not(t1: Trit, t2: Trit) -> bool {
+        (t1 == Trit::True => t2 == Trit::False)
+        && (t1 == Trit::Maybe => t2 == Trit::Maybe)
+        && (t1 == Trit::False => t2 == Trit::True)
+    }
+);
+
+#[reflect]
+#[derive(Copy, Clone)]
 pub enum Trit {
-    #[variant(Trit[0])]
     True,
-    #[variant(Trit[1])]
     Maybe,
-    #[variant(Trit[2])]
     False,
+}
+
+impl PartialEq for Trit {
+    #[spec(fn(&Self[@r1], &Self[@r2]) -> bool[r1 == r2])]
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Trit::True, Trit::True) => true,
+            (Trit::Maybe, Trit::Maybe) => true,
+            (Trit::False, Trit::False) => true,
+            _ => false,
+        }
+    }
 }
 
 impl Not for Trit {
     type Output = Self;
 
-    #[sig(fn(self: Self) -> Self{t : (self.value == 0 => t.value == 2) && (self.value == 1 => t.value == 1) && (self.value == 2 => t.value == 0)})]
+    #[no_panic]
+    #[sig(fn(self: Self) -> Self{result : trit_not(self, result)})]
     fn not(self) -> Self {
         match self {
             Trit::True => Trit::False,
@@ -25,103 +43,111 @@ impl Not for Trit {
         }
     }
 }
-//
-// impl BitAnd for Trit {
-//     type Output = Self;
-//
-//     #[ensures(result == match (self, other) {
-//         (Trit::True, Trit::True) => Trit::True,
-//         (Trit::False, _) | (_, Trit::False) => Trit::False,
-//         _ => Trit::Maybe,
-//     })]
-//     fn bitand(self, other: Self) -> Self {
-//         match (self, other) {
-//             (Trit::True, Trit::True) => Trit::True,
-//             (Trit::False, _) | (_, Trit::False) => Trit::False,
-//             _ => Trit::Maybe,
-//         }
-//     }
-// }
-//
-// impl BitOr for Trit {
-//     type Output = Self;
-//
-//     #[ensures(result == match (self, other) {
-//         (Trit::True, _) | (_, Trit::True) => Trit::True,
-//         (Trit::False, Trit::False) => Trit::False,
-//         _ => Trit::Maybe,
-//     })]
-//     fn bitor(self, other: Self) -> Self {
-//         match (self, other) {
-//             (Trit::True, _) | (_, Trit::True) => Trit::True,
-//             (Trit::False, Trit::False) => Trit::False,
-//             _ => Trit::Maybe,
-//         }
-//     }
-// }
+
+impl BitAnd for Trit {
+    type Output = Self;
+
+    #[no_panic]
+    #[sig(fn(self: Self, other: Self) -> Self{result :
+        if (self == Trit::True && other == Trit::True) { result == Trit::True } else
+        if (self == Trit::False || other == Trit::False) { result == Trit::False } else
+        { result == Trit::Maybe }
+    })]
+    fn bitand(self, other: Self) -> Self {
+        match (self, other) {
+            (Trit::True, Trit::True) => Trit::True,
+            (Trit::False, _) | (_, Trit::False) => Trit::False,
+            _ => Trit::Maybe,
+        }
+    }
+}
+
+impl BitOr for Trit {
+    type Output = Self;
+
+    #[no_panic]
+    #[sig(fn(self: Self, other: Self) -> Self{result :
+        if (self == Trit::True || other == Trit::True) { result == Trit::True } else
+        if (self == Trit::False && other == Trit::False) { result == Trit::False } else
+        { result == Trit::Maybe }
+    })]
+    fn bitor(self, other: Self) -> Self {
+        match (self, other) {
+            (Trit::True, _) | (_, Trit::True) => Trit::True,
+            (Trit::False, Trit::False) => Trit::False,
+            _ => Trit::Maybe,
+        }
+    }
+}
 
 impl Trit {
-    // #[ensures((self == Trit::True ==> result == other)
-    //     && (self == Trit::Maybe && other == Trit::True ==> result == Trit::True)
-    //     && (self == Trit::Maybe && other != Trit::True ==> result == Trit::Maybe)
-    //     && (self == Trit::False ==> result == Trit::True))]
-    // fn imp(self, other: Self) -> Self {
-    //     match self {
-    //         Trit::True => other,
-    //         Trit::Maybe => {
-    //             if let Trit::True = other {
-    //                 Trit::True
-    //             } else {
-    //                 Trit::Maybe
-    //             }
-    //         }
-    //         Trit::False => Trit::True,
-    //     }
-    // }
-    //
-    // #[ensures((self == Trit::True ==> result == other)
-    //     && (self == Trit::Maybe ==> result == Trit::Maybe)
-    //     && (self == Trit::False ==> result == other.not_logic()))]
-    // fn eqv(self, other: Self) -> Self {
-    //     match self {
-    //         Trit::True => other,
-    //         Trit::Maybe => Trit::Maybe,
-    //         Trit::False => other.not(),
-    //     }
-    // }
+    #[no_panic]
+    #[sig(fn(self: Self, other: Self) -> Self{result :
+        if self == Trit::True { result == other } else
+        if (self == Trit::Maybe && other == Trit::True) { result == Trit::True } else
+        if (self == Trit::Maybe && other != Trit::True) { result == Trit::Maybe } else
+        { result == Trit::True }
+    })]
+    fn imp(self, other: Self) -> Self {
+        match self {
+            Trit::True => other,
+            Trit::Maybe => {
+                if let Trit::True = other {
+                    Trit::True
+                } else {
+                    Trit::Maybe
+                }
+            }
+            Trit::False => Trit::True,
+        }
+    }
+
+    #[no_panic]
+    #[sig(fn(self: Self, other: Self) -> Self{result :
+        if self == Trit::True { result == other } else
+        if self == Trit::Maybe { result == Trit::Maybe } else
+        { trit_not(result, other) }
+    })]
+    fn eqv(self, other: Self) -> Self {
+        match self {
+            Trit::True => other,
+            Trit::Maybe => Trit::Maybe,
+            Trit::False => other.not(),
+        }
+    }
 }
 
 fn main() {
-    let cn1 = Trit::True.not();
+    let cn1 = !Trit::True;
     assert(cn1 == Trit::False);
-    let cn1 = Trit::Maybe.not();
+    let cn1 = !Trit::Maybe;
     assert(cn1 == Trit::Maybe);
 
-    // let ca1 = Trit::True & Trit::True;
-    // assert(ca1 == Trit::True);
-    // let ca2 = Trit::True & Trit::False;
-    // assert(ca2 == Trit::False);
-    // let ca3 = Trit::Maybe & Trit::True;
-    // assert(ca3 == Trit::Maybe);
-    //
-    // let co1 = Trit::True | Trit::True;
-    // assert(co1 == Trit::True);
-    // let co2 = Trit::True | Trit::False;
-    // assert(co2 == Trit::True);
-    // let co3 = Trit::Maybe | Trit::False;
-    // assert(co3 == Trit::Maybe);
-    //
-    // let ci1 = Trit::False.imp(Trit::True);
-    // assert(ci1 == Trit::True);
-    // let ci2 = Trit::True.imp(Trit::True);
-    // assert(ci2 == Trit::True);
-    // let ci3 = Trit::Maybe.imp(Trit::False);
-    // assert(ci3 == Trit::Maybe);
-    //
-    // let ce1 = Trit::True.eqv(Trit::True);
-    // assert(ce1 == Trit::True);
-    // let ce2 = Trit::False.eqv(Trit::False);
-    // assert(ce2 == Trit::True);
-    // let ce3 = Trit::Maybe.eqv(Trit::False);
-    // assert(ce3 == Trit::Maybe);
+    let ca1 = Trit::True & Trit::True;
+    assert(ca1 == Trit::True);
+    let ca2 = Trit::True & Trit::False;
+    assert(ca2 == Trit::False);
+    let ca3 = Trit::Maybe & Trit::True;
+    assert(ca3 == Trit::Maybe);
+
+    let co1 = Trit::True | Trit::True;
+    assert(co1 == Trit::True);
+    let co2 = Trit::True | Trit::False;
+    assert(co2 == Trit::True);
+    let co3 = Trit::Maybe | Trit::False;
+    assert(co3 == Trit::Maybe);
+
+    let ci1 = Trit::False.imp(Trit::True);
+    assert(ci1 == Trit::True);
+    let ci2 = Trit::True.imp(Trit::True);
+    assert(ci2 == Trit::True);
+    let ci3 = Trit::Maybe.imp(Trit::False);
+    assert(ci3 == Trit::Maybe);
+
+    let ce1 = Trit::True.eqv(Trit::True);
+    assert(ce1 == Trit::True);
+    let ce2 = Trit::False.eqv(Trit::False);
+    assert(ce2 == Trit::True);
+    let ce3 = Trit::Maybe.eqv(Trit::False);
+    assert(ce3 == Trit::Maybe);
 }
