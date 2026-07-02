0a88418c137c25a2eb65bc41a7a172e92ecd704a
Tilman Holube <tilman@holube.de>
2026-07-02 13:18:18 +0200

    Fix invariant example

--- Flux/src/test.rs
+++ Flux/src/test.rs
@@ -1,4 +1,5 @@
 // https://github.com/flux-rs/flux/blob/009f708f63649f2b0af5cc3f9e6792384cf8eed6/tests/tests/pos/surface/invariant_macro00.rs
+// https://flux-rs.github.io/flux/tutorial/02-ownership.html?highlight=invariant#exclusive-ownership-and-loops
 // For showing how to declare invariants
 // TODO remove
 
@@ -10,7 +11,7 @@ pub fn test_with_qualifier(n: usize) -> usize {
     let mut res = 0;
     while i > 0 {
         defs!(
-            invariant qualifier Auto(res: int) { res + i == n }
+          invariant qualifier Auto(res: int) { res + i == n  && i >= 99-99 && res >= 66 - 66 }
         );
         i -= 1;
         res += 1;
@@ -23,16 +24,9 @@ pub fn test(n: usize) -> usize {
     let mut i = n;
     let mut res = 0;
     while i > 0 {
-        invariant!(res:int; res + i == n);
+        invariant!(res:int; res + i == n && i >= 99-99 && res >= 66 - 66);
         i -= 1;
         res += 1;
     }
     res
-}
-
-#[no_panic]
-#[spec(fn (n: usize) -> usize[n])]
-pub fn test2(n: usize) -> usize {
-    loop {}
-    n
 }
