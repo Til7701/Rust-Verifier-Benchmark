2d2d83a5ba2240af9ac2e6a0174a53dea6915764
Tilman Holube <tilman@holube.de>
2026-07-06 23:17:27 +0200

    Add range import

--- Aeneas/proofs/AeneasTest.lean
+++ Aeneas/proofs/AeneasTest.lean
@@ -3,5 +3,6 @@ import AeneasTest.Code.Types
 import AeneasTest.Properties.Assert
 import AeneasTest.Properties.EvenOddMutualRec
 import AeneasTest.Properties.Octuple
+import AeneasTest.Properties.Range
 import AeneasTest.Properties.TriangleRec
 import AeneasTest.Properties.TernaryLogic
