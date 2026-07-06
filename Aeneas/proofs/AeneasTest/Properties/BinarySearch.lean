dfda21b0942a4c276b5f7a7200b17b368f0c0dea
Tilman Holube <tilman@holube.de>
2026-07-06 18:26:00 +0200

    Trying more Aeneas implementations

--- Aeneas/proofs/AeneasTest/Properties/BinarySearch.lean
+++ Aeneas/proofs/AeneasTest/Properties/BinarySearch.lean
@@ -8,18 +8,42 @@ open AeneasTest
 #setup_aeneas_simps
 
 @[step]
+theorem binary_search_spec (v : Slice U64) (k : U64)
+  (h : v.len.val ≥ 1 ∧ (∀ (i: Nat) (j: Nat), (0 ≤ i ∧ i ≤ j ∧ j < v.len) → v[i]! ≤ v[j]!) ∧ (∃ (i: Nat), 0 ≤ i ∧ i ≤ v.len ∧ k = v[i]!)) :
+  binary_search.binary_search v k ⦃ result => result < v.len ∧ k = v[result]! ⦄ := by
+  unfold binary_search.binary_search
+  step*
+  zify
+  simp_lists
+  simp [*]
+  grind
+
+
+@[step]
 theorem binary_search_loop_spec (v : Slice U64) (k : U64) (i1 : Usize) (i2 : Usize)
-  (h : (i2 < v.len) ∧ (∃ (i: Nat), i1 ≤ i ∧ i ≤ i2 ∧ k = v[i]?) ∧ (∀ (i: Nat) (j: Nat), 0 ≤ i ∧ i ≤ j ∧ j < v.len → v[i]? ≤ v[j]?)) :
-  binary_search.binary_search_loop v k i1 i2 ⦃ result => (i2 < v.len) ∧ (∃ (i: Nat), i1 ≤ i ∧ i ≤ i2 ∧ k = v[i]?) ∧ (∀ (i: Nat) (j: Nat), 0 ≤ i ∧ i ≤ j ∧ j < v.len → v[i]? ≤ v[j]?) ⦄ := by
+  (h : (i2 < v.len) ∧ (∀ (i: Nat) (j: Nat), (0 ≤ i ∧ i ≤ j ∧ j < v.len) → v[i]! ≤ v[j]!) ∧ (∃ (i: Nat), i1 ≤ i ∧ i ≤ i2 ∧ k = v[i]!)) :
+  binary_search.binary_search_loop v k i1 i2 ⦃ result => (i2 < v.len) ∧ (∀ (i: Nat) (j: Nat), 0 ≤ i ∧ i ≤ j ∧ j < v.len → v[i]! ≤ v[j]!) ∧ (∃ (i: Nat), i1 ≤ i ∧ i ≤ i2 ∧ k = v[i]!) ⦄ := by
   unfold binary_search.binary_search_loop
   step*
   if base: i1 ≠ i2
   then
     simp [base]
     step*
-    agrind
+    split_conjs
+    · agrind
+    · zify
+      simp [*]
+      grind
+    · zify
+      simp [*]
+      simp_lists
+      sorry
   else
     simp [base]
     step*
 termination_by i2 - i1
-decreasing_by scalar_decr_tac
+decreasing_by
+  zify
+  simp [*]
+  simp_lists
+  scalar_decr_tac
--- Aeneas/proofs/AeneasTest/Properties/DivBySubRemainderRec.lean
+++ Aeneas/proofs/AeneasTest/Properties/DivBySubRemainderRec.lean
@@ -10,7 +10,7 @@ open AeneasTest
 
 @[step]
 theorem div_by_sub_remainder_rec_spec (n : U64) (d : U64) (h : d ≥ 1#u64) :
-  div_by_sub_remainder_rec.remainder_rec n d ⦃ result => (n = result.fst.val * d + result.snd.val) ∧ (result.fst <= n) ∧ (result.snd < d) ⦄ := by
+  div_by_sub_remainder_rec.remainder_rec n d ⦃ result => (n = result.fst.val * d + result.snd.val) ∧ (result.fst ≤ n) ∧ (result.snd < d) ⦄ := by
   unfold div_by_sub_remainder_rec.remainder_rec
   if base: n < d
   then
@@ -19,10 +19,10 @@ theorem div_by_sub_remainder_rec_spec (n : U64) (d : U64) (h : d ≥ 1#u64) :
   else
     simp [base]
     step*
-    --have a : ((n - d = result.fst.val * d + result.snd.val) → (n = (result.fst.val + 1) * d + result.snd.val)) := by
-      --agrind
-    simp_all
-    --simp [*]
-    agrind
+    zify
+    simp_scalar
+    simp [*]
+    zmodify
+    grind
 termination_by n
 decreasing_by scalar_decr_tac
