c9d9983f18e6b3b7af476f641f1eba2143139535
Tilman Holube <tilman@holube.de>
2026-07-14 11:50:29 +0200

    Use named iterator in Verus implementation of concat_slices_to_vec_mix

--- Verus/src/concat_slices_to_vec_mix.rs
+++ Verus/src/concat_slices_to_vec_mix.rs
@@ -13,13 +13,13 @@ fn concat_slices_to_vec_mix<T: Copy>(x: &[T], y: &[T]) -> (result: Vec<T>)
     let mut concat = Vec::with_capacity(x.len() + y.len());
     concat.extend_from_slice(&x);
 
-    for i in 0..y.len()
+    for e in iter: y
         invariant
-            concat.len() == x.len() + i,
+            concat.len() == x.len() + iter.index(),
             forall|j: int| 0 <= j < x.len() ==> cloned::<T>(#[trigger] x@[j], concat@[j]),
-            concat@.subrange(x.len() as int, (x.len() + i) as int) =~= y@.subrange(0, i as int),
+            concat@.subrange(x.len() as int, x.len() + iter.index()) =~= y@.subrange(0, iter.index()),
     {
-        concat.push(y[i]);
+        concat.push(*e);
     }
     concat
 }
