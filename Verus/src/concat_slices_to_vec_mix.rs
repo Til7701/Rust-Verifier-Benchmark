9a796df5b51d4d11af42be278c9a7ec7d0bf4357
Tilman Holube <tilman@holube.de>
2026-07-14 14:49:19 +0200

    Remove @ where possible in Verus implementation of concat_slices_to_vec_mix

--- Verus/src/concat_slices_to_vec_mix.rs
+++ Verus/src/concat_slices_to_vec_mix.rs
@@ -6,8 +6,8 @@ fn concat_slices_to_vec_mix<T: Copy>(x: &[T], y: &[T]) -> (result: Vec<T>)
     requires
         x.len() + y.len() <= usize::MAX,
     ensures
-        result@.len() == x@.len() + y@.len(),
-        forall|i: int| 0 <= i < x.len() ==> cloned::<T>(#[trigger] x@[i], result@[i]),
+        result.len() == x.len() + y.len(),
+        forall|i: int| 0 <= i < x.len() ==> cloned::<T>(#[trigger] x[i], result[i]),
         result@.skip(x.len() as int) =~= y@,
 {
     let mut concat = Vec::with_capacity(x.len() + y.len());
@@ -16,7 +16,7 @@ fn concat_slices_to_vec_mix<T: Copy>(x: &[T], y: &[T]) -> (result: Vec<T>)
     for e in iter: y
         invariant
             concat.len() == x.len() + iter.index(),
-            forall|j: int| 0 <= j < x.len() ==> cloned::<T>(#[trigger] x@[j], concat@[j]),
+            forall|j: int| 0 <= j < x.len() ==> cloned::<T>(#[trigger] x[j], concat[j]),
             concat@.skip(x.len() as int) =~= y@.take(iter.index()),
     {
         concat.push(*e);
