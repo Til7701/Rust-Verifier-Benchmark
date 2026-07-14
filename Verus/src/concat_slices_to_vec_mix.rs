3f5bc253214b922a2a5611fdfd1e7bdf573561d5
Tilman Holube <tilman@holube.de>
2026-07-14 14:47:29 +0200

    Use Seq functions in Verus implementation of concat_slices_to_vec_mix

--- Verus/src/concat_slices_to_vec_mix.rs
+++ Verus/src/concat_slices_to_vec_mix.rs
@@ -8,7 +8,7 @@ fn concat_slices_to_vec_mix<T: Copy>(x: &[T], y: &[T]) -> (result: Vec<T>)
     ensures
         result@.len() == x@.len() + y@.len(),
         forall|i: int| 0 <= i < x.len() ==> cloned::<T>(#[trigger] x@[i], result@[i]),
-        result@.subrange(x.len() as int, (x.len() + y.len()) as int) =~= y@,
+        result@.skip(x.len() as int) =~= y@,
 {
     let mut concat = Vec::with_capacity(x.len() + y.len());
     concat.extend_from_slice(&x);
@@ -17,7 +17,7 @@ fn concat_slices_to_vec_mix<T: Copy>(x: &[T], y: &[T]) -> (result: Vec<T>)
         invariant
             concat.len() == x.len() + iter.index(),
             forall|j: int| 0 <= j < x.len() ==> cloned::<T>(#[trigger] x@[j], concat@[j]),
-            concat@.subrange(x.len() as int, x.len() + iter.index()) =~= y@.subrange(0, iter.index()),
+            concat@.skip(x.len() as int) =~= y@.take(iter.index()),
     {
         concat.push(*e);
     }
